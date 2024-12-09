5960a8edce9bed50d383005589b7768b15451cf4
T Gwatipedza <146441267+TerrenceGwati@users.noreply.github.com>
2024-12-09 17:13:02 +0000

    Update page1_window.cpp

--- src/pages/page1_window.cpp
+++ src/pages/page1_window.cpp
@@ -1,9 +1,26 @@
 #include "pages/page1_window.hpp"
+#include <QPushButton>
+#include <QComboBox>
+#include <QTableWidget>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QChart>
+#include <QtCharts/QValueAxis>
+#include <QtCharts/QCategoryAxis>
+#include <QVBoxLayout>
 
+using namespace QtCharts;
+
+//constructor
 Page1Window::Page1Window(QWidget* parent)
         : QWidget(parent),
           pageLabel(new QLabel(this)),
-          layout(new QVBoxLayout(this))
+          layout(new QVBoxLayout(this)),
+          litterTable(new QTableWidget(this)),
+          chartView(new QChartView(this)),
+          filterComboBox(new QComboBox(this)),
+          refreshButton(new QPushButton("Refresh", this))
+          
 {
     pageLabel->setText("This is Page 1. Display analysis and visualization here.");
     pageLabel->setAlignment(Qt::AlignCenter);
