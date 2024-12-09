0dcbcab09c4806b20a8a2bed363fdacd7b475f53
T Gwatipedza <146441267+TerrenceGwati@users.noreply.github.com>
2024-12-09 16:35:25 +0000

    Update page1_window.hpp

--- include/pages/page1_window.hpp
+++ include/pages/page1_window.hpp
@@ -4,17 +4,39 @@
 #include <QWidget>
 #include <QLabel>
 #include <QVBoxLayout>
+#include <QTableWidget>
+#include <QChartView>
+#include <map>
+#include <string>
+#include <vector>
+#include <"dataset.hpp">
+
+QT_BEGIN_NAMESPACE
+class QPushButton;
+class QComboBox;
+QT_END_NAMESPACE
 
 class Page1Window : public QWidget {
 Q_OBJECT
 
 public:
+    //constructor
     explicit Page1Window(QWidget* parent = nullptr);
+    //Destructor
     ~Page1Window();
 
+private slots:
+    //slot to update the displayed data based on filters
+    void updateDashboard();
+
 private:
+    //ui elements
     QLabel* pageLabel;
     QVBoxLayout* layout;
+    QTableWidget* litterTable;
+    QChartView* chartView;
+    QComboBox* filterComboBox;
+    QPushButton* 
 };
 
 #endif // PAGE1_WINDOW_HPP
